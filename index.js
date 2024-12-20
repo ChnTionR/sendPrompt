bbd83bcaa11acc6c0c4177b72e705a72ec3b5559
Çınar Kaya <cnrkaya008@gmail.com>
2024-12-20 23:12:57 +0300

    added functionality

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const http = request('http');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
+
     if(req.url === '/'){
-        res.statusCode = 200;
-        console.log(prompt("send me a message"));
-        res.end();
+
+        if(req.method === 'GET'){
+
+            fs.readFile(path.join(__dirname, 'public', 'site.html'), 'utf8', (err, data)=>{
+
+                if(err){
+                    res.statusCode = 500;
+                    res.end('error occured when loading page');
+                }else{
+                    res.statusCode = 200;
+                    res.end(data);
+                }
+            });
+        }
+
+        if(req.method === 'POST'){
+            let message = '';
+            req.on('data', (chunk)=>{message += chunk;});
+            req.on('end', ()=>{
+                console.log(message);
+                res.end();
+            });
+        }
     }else{
         res.StatusCode = 404;
         res.end('page not found');
     }
+
+    
 });
 
-server.listen(process.env.PORT || 3000, ()=>{console.log('server running on' + process.env.PORT);});
+server.listen(process.env.PORT || 3000, ()=>{console.log('server running on ' + (process.env.PORT || 3000));});
